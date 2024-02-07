4ccb5fb02176ebc3c931e6683bfe0e6c1f14dd53
kummy22 <kummykonnectzit@gmail.com>
2024-02-07 22:46:28 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,20 @@
+
+const parent = React.createElement("div", {id:"parent"},
+[React.createElement("div", {id:"child"}, 
+React.createElement("h1", {}, "This is the h1 tag"),
+React.createElement("h2", {}, "This is the h2 tag")),
+React.createElement("div", {id:"child2"}, 
+React.createElement("h1", {}, "This is the h1 tag"),
+React.createElement("h2", {}, "This is the h2 tag"))])
+
+
+
+
+
+
+
+
+
+      const root =  ReactDOM.createRoot(document.getElementById("root"));
+
+      root.render(parent);
